6e5f631d6a819f68797c034fded055163300a74c
bryn9696 <bryn2009@live.co.uk>
2022-07-05 13:29:36 +0100

    created all component files and linked up

--- portfolio/src/app.jsx
+++ portfolio/src/app.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Header from './components/header/header.jsx'
+import Nav from './components/nav/nav.jsx'
+import About from './components/about/about.jsx'
+import Experience from './components/experience/experience.jsx'
+import Services from './components/services/services.jsx'
+import Portfolio from './components/portfolio/portfolio.jsx'
+import Testimonials from './components/testimonials/testimonials.jsx'
+import Contact from './components/contact/contact.jsx'
+import Footer from './components/footer/footer.jsx'
 
-const app = () => {
+const App = () => {
   return (
-    <div>app</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
-export default app
+export default App
