131367ebe491259840a0e1d1ee2765b6b2c1da1a
bryn9696 <bryn2009@live.co.uk>
2022-07-20 15:51:33 +0100

    nav bar done

--- portfolio/src/components/nav/nav.jsx
+++ portfolio/src/components/nav/nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {BiUserCircle} from 'react-icons/bi'
+import {BsJournalBookmarkFill} from 'react-icons/bs'
+import {RiServiceLine} from 'react-icons/ri'
+import {AiOutlineContacts} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserCircle/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsJournalBookmarkFill/></a>
+      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineContacts/></a>
+    </nav>
   )
 }
 
