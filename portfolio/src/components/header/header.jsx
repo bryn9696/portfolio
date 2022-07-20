9f76eee2fd50ee8b529f68c6e2898408ce2c5306
bryn9696 <bryn2009@live.co.uk>
2022-07-20 11:09:14 +0100

    added buttons to header

--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello</h5>
+        <h5>Hello I'm</h5>
+        <h1>Bryn Williams</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
       </div>
     </header>
   )
