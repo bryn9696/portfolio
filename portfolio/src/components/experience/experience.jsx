9a34c4995024bee2f896c2596a962e5b02db326d
bryn9696 <bryn2009@live.co.uk>
2022-07-21 10:57:49 +0100

    experience section beginning

--- portfolio/src/components/experience/experience.jsx
+++ portfolio/src/components/experience/experience.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './experience.css'
+import {BiBadgeCheck} from 'react-icons/bi'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>My Skills</h5>
+      <h1>My Experience</h1>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>HTML</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>CSS</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>React</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>Ruby / Ruby on Rails</h4>
+              <small className='text-light'>Intermidiate</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>Python</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>Node JS</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+            <article className='experience__Details'>
+              <BiBadgeCheck/>
+              <h4>C#</h4>
+              <small className='text-light'>Beginner</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
