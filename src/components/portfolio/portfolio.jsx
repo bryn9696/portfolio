d3471553e2c8fb6e1055e1e066e0ce7dac93799a
Bryn Williams <bryn2009@live.co.uk>
2023-09-22 17:45:59 +0100

    css for phones

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -5,8 +5,8 @@ import SCREEN from '../../assets/screen.png'
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-        <h5>My Personal Work</h5>
-        <h2>Portfolio</h2>
+      <h5>My Personal Work</h5>
+      <h2>Portfolio</h2>
       <div className="portfolio__container">
 
       <article className='portfolio__item'>
